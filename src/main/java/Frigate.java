ab9c63446704453aa9e019d593dde46e0145f617
ana <andelamo@myuax.com>
2023-04-13 16:41:43 +0200

    Modificaciones clase Frigate

--- src/main/java/Frigate.java
+++ src/main/java/Frigate.java
@@ -2,7 +2,8 @@ import java.awt.*;
 
 public class Frigate extends Ship{
     private int SIZE = 3;
+
     public Frigate(Point start, Point end) {
-        super(3, start, end);
+        super(start, end);
     }
 }
